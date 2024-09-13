4f70dc15561bc7006e3e1fc6b6dbe20a4178f83c
ppraachiii <ppraachiii@gmail.com>
2024-09-13 05:25:52 +0000

    first commit

---
+++ pra_q_1.c
@@ -0,0 +1,20 @@
+//program to check odd and even numbers
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    printf("enter your number: ");
+    scanf("%d",&i);
+    if (i%2==0)
+    {
+        printf("your number is even !");
+    }
+    else
+    {
+        printf("your number is odd");
+    }
+    
+
+    return 0;
+}
