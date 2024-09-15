9da8f5091c8130fa4b049469f82dafe58dedda3c
ppraachiii <ppraachiii@gmail.com>
2024-09-15 03:16:37 +0000

    11th commit

---
+++ .vscode/pra_q_11.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+ {
+    int n;
+    
+    
+    printf("Enter the number :");
+    scanf("%d", &n);
+
+   
+    if (n <= 0) 
+    {
+        printf("Please enter a positive integer.\n");
+        return 1; 
+    }
+    
+    long long first = 0, second = 1, next;
+    
+    printf("Fibonacci series up to %d terms:\n", n);
+
+    
+    if (n >= 1)
+    {
+        printf("%lld ", first);
+    }
+
+    
+    for (int i = 1; i < n; i++) 
+    {
+        printf("%lld ", second);
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    printf("\n"); 
+    return 0;
+}
